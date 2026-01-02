d348fc58e69ffb27aa2902326aa001c16fa2e02e
Vishal Gupta <webervishal@gmail.com>
2026-01-02 21:51:23 +0530

    Q1.

---
+++ Q1. zlib-stream/compress.js
@@ -0,0 +1,28 @@
+// 1. Create a node.js application to demostrate the use to Zlib and stream modules
+// 2.  Create a database new1 in mongodb
+// - Create a new collection student in new1
+// - insert 6 documents in student for regno, name and marks
+// - Update the marks of the student whosse regno is 2
+// - show the marks of those students in student who have more than 80 marks
+
+
+import fs from 'fs';
+import zlib from 'zlib';
+
+const fs =  require("fs");
+const zlib = require("zlib");
+
+// Create Streams
+const readStream = fs.createReadStream("input.txt");
+const writeStream = fs.createWriteStream("input.txt.gz")
+
+// Create gzip stream
+const qzip = zlib.createGzip();
+
+// Pipe streams 
+readStream
+    .pipe(gzip)
+    .pipe(writeStream)
+    .on("finish", () => {
+        console.log("File comparessed Successfullt!");
+});
---
+++ Q1. zlib-stream/decompress.js
@@ -0,0 +1,25 @@
+// 📌 Concept
+
+// Stream → reads data in chunks (efficient for large files)
+
+// Zlib → compresses/decompresses data (gzip)
+
+
+import fs from 'fs';
+import zlib, { gzip } from 'zlib';
+
+// Create Streams 
+const readStream = fs.createReadStream("input.txt.gz");
+const writeStream = fs.createWriteStream("output.txt");
+
+// Create gunzip stream
+const gunzip = zlib.createGunzip();
+
+// Pipe Streams
+
+readStream
+    .pipe(gunzip)
+    .pipe(writeStream)
+    .on("finish", () => {
+        console.log("File Decompressed successfully!");
+});
