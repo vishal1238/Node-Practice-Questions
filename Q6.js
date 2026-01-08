3a03c3bc2f2e25ced0ea7099f518e83c5fd6d0ec
Vishal Gupta <webervishal@gmail.com>
2026-01-08 19:09:31 +0530

    Q6 update

--- Q6.js
+++ Q6.js
@@ -1,1 +1,31 @@
 // Implement a custom express middleware that logs request method, URL and timeStamp for every incoming request. Apply the middleware globally using appendFile.use();
+
+import express from "express";
+const app = express();
+
+//Custom middleware
+const loggerMiddleware = (req, res, next) => {
+    const timeStamp = new Date().toLocaleString();
+
+    console.log(
+        `[${timeStamp}] ${req.method} ${req.url}`
+    );
+    next(); // move to next middleware / route
+};
+
+//Apply middleware globally
+app.use(loggerMiddleware);
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+// Another route
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+//Start server
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
