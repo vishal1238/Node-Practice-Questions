5588d21b2a79b42df1bee25db7cf8e5af1b64a7d
Vishal Gupta <webervishal@gmail.com>
2026-01-08 14:16:40 +0530

    Q5

---
+++ Q5.js
@@ -0,0 +1,2 @@
+// Create a basic website using Express that displays a registration from using a GET request and process the submitted form data using POST. use body-parser middleware to extract user input. 
+
